9e970c6e8402f1f38a38dd18b4377365ca5acea9
firatmikyaz <66138781+firatmikyaz@users.noreply.github.com>
2020-06-05 01:02:17 +0300

    Add project files.

---
+++ Console'da_Tek_Çift_Sayi_Bulma/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_da_Tek_Çift_Sayi_Bulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.WriteLine("Bir sayı giriniz?");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi % 2 == 0 && sayi > 0)
+            {
+                Console.Write("Sayı pozitif çift sayıdır.");
+            }
+            else if (sayi % 2 == 0 && sayi < 0)
+            {
+                Console.Write("Sayı negatif çift sayıdır.");
+            }
+            else if (sayi % 2 != 0 && sayi > 0)
+            {
+                Console.Write("Sayı pozitif tek sayıdır.");
+            }
+            else if (sayi % 2 != 0 && sayi < 0)
+            {
+                Console.Write("Sayı negatif tek sayıdır.");
+            }
+            else if ( sayi == 0) 
+            {
+                Console.Write("Sayı ne negatif nede pozitif bir sayıdır.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
